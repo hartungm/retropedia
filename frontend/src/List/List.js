95d342f48d909f9a44b9f9d6bd5da1e83950a101
Michael Hartung <hartungm4@gmail.com>
2019-07-31 13:57:56 -0400

    Now displaying description and intent if exists, also fixed errors

--- frontend/src/List/List.js
+++ frontend/src/List/List.js
@@ -24,14 +24,37 @@ export class List extends React.Component {
 
     renderIdea(idea) {
         return(
-            <Card className="card">
+            <Card className="card" key={idea.key}>
                 <CardContent>
-                    <Typography className="title" color="textSecondary" gutterBottom>
+                    <Typography className="title" variant="h5" component="h2">
                         {idea.name}
                     </Typography>
+                    <Typography className="description-title" color="textSecondary">
+                        Description:
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                        {idea.description}
+                    </Typography>
+                    {this.renderIntent(idea.intent)}
                 </CardContent>
             </Card>
         );
+    }
+
+    renderIntent(intent) {
+        console.log(intent);
+        if(intent !== null && intent !== undefined) {
+            return (
+                <div className="intent">
+                    <Typography className="intent-title" color="textSecondary">
+                        Intent:
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                        {intent}
+                    </Typography>
+                </div>
+            );
+        }
     }
 }
 
